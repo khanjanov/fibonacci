9c59b5a3cf91fa4f684cf7e74646f04a6ad35bb3
khanjanov <you@example.com>
2023-02-03 15:04:53 +0400

    done

---
+++ main.js
@@ -0,0 +1,26 @@
+//! n-ci fibonacci ededi
+// let a = prompt("enter a number : ");
+// let b = 1;
+// let c = 1;
+// if (a == 1) console.log(b);
+// if (a == 2) console.log(c);
+
+// for (let i = 2; i < a; i++) {
+//   let d = b;
+//   b = c;
+//   c = b + d;
+//   if (a - 1 == i) {
+//     console.log(`%c ${c}`, "color: skyblue");
+//   }
+// }
+
+//! n-e qeder fibonacci ededleri
+// function fibo(n) {
+//   let fiboArr = [0, 1];
+//   if (n < 2) return fiboArr;
+//   for (let i = 2; i < n; i++) {
+//     fiboArr[i] = fiboArr[i - 1] + fiboArr[i - 2];
+//   }
+//   return fiboArr;
+// }
+// console.log(fibo(5));
